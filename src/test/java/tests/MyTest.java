1d0957e1b5fafd2d78194a60f90b70d66c4549bb
Руслан <ruslznzhuminov@mail.ru>
2022-02-04 22:55:58 +0300

    first commit

---
+++ src/test/java/tests/MyTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class MyTest {
+
+    @Test
+    public void userCanLoginByUsername() {
+        open("https://www.google.ru/");
+        $(By.name("q")).setValue("Ведьмак").pressEnter();
+        $(By.linkText("Ведьмак")).click();
+    }
+}
